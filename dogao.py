ec65de8119dd2c755676824a634137e1f42fde90
Warley Pires <warleypires@maxprocess.com.br>
2022-01-24 11:07:17 -0300

    primeiro do dogao

---
+++ dogao.py
@@ -0,0 +1,7 @@
+
+preco_dog = float(input("Digite o valor do Dogao:"))
+preco_suco = float(input("Digite o valor do suco:"))
+dog = float(input ("Digite o numero pedido de Dogs:"))
+suco = float(input("Digite o numero pedido de sucos:"))
+total = ((preco_dog * dog) + (preco_suco * suco))
+print ("Total da conta é:",(total))
